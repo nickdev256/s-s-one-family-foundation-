2f2729cfc897c6bb90340e4b831b668f2e140e59
Nicholas Ssentongo <nickclas256@gmail.com>
2026-06-10 15:10:15 +0300

    debug paypal env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,10 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 import App from "./App";
 import "./index.css";
-
+console.log(
+  "PAYPAL CLIENT ID:",
+  import.meta.env.VITE_PAYPAL_CLIENT_ID
+);
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
